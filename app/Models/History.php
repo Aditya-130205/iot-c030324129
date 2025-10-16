fc414c6d213afd7691c1d615bd39d89e8b82ece4
Aditya-130205 <4dity4aryandi46@gmail.com>
2025-10-16 13:45:33 +0800

    Membuat model lamp

---
+++ app/Models/History.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
+
+class History extends Model
+{
+    use HasFactory;
+    protected $fillable = ['lamp_id', 'status'];
+}
